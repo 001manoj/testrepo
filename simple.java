6ac1cbe0da0854840696ac4170df821cf78a2ec3
Manoj Kumar Pal <115406198+001manoj@users.noreply.github.com>
2023-04-05 17:38:50 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,7 +1,7 @@
 class Simple{
 public static void main(String args[]){
 
-System.out.println("Welcome to our first jenkins project integrated with github");
+System.out.println("Welcome to our first jenkins project integrated with github by manoj");
 
 }
 }
